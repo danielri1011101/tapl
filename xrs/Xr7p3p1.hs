8b42a17a4f9228d282a45cb73ddb2e1909a9899e
danielri1011101 <100494224+danielri1011101@users.noreply.github.com>
2024-05-06 14:43:45 -0500

    Update and rename xr7p3p1.hs to Xr7p3p1.hs

---
+++ xrs/Xr7p3p1.hs
@@ -0,0 +1,24 @@
+import Core
+
+-- Exercise 7.3.1: Implement evaluation of terms using big-step semantics.
+-- can it be written using _where_ constructs instead of _let_ bindings and _case_ expressions?
+
+bigstepEval :: NlTerm -> Maybe NlTerm 
+bigstepEval t
+  | isval t = Just t
+bigstepEval (TmApp t1 t2)
+  | maybe 
+      False 
+        isval ( let v1 = bigstepEval t1
+                    v2 = bigstepEval t2
+                in case (v1,v2) of (_,Nothing) -> Nothing
+                                   (Nothing,_) -> Nothing
+                                   (Just u1, Just u2) ->
+                                      bigstepEval (termSubstTop u2 u1)
+              ) = let v1 = bigstepEval t1
+                      v2 = bigstepEval t2
+                  in case (v1,v2) of (_,Nothing) -> Nothing
+                                     (Nothing,_) -> Nothing
+                                     (Just u1, Just u2) ->
+                                        Just (bigstepEval (termSubstTop u2 u1))
+bigstepEval _ = Nothing
