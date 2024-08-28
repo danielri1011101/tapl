9820f8d298198bd1fcda34d15178272fb564f5dd
danielri1011101 <100494224+danielri1011101@users.noreply.github.com>
2024-08-28 18:10:16 -0500

    Update Core.hs

--- simpleTypes/Core.hs
+++ simpleTypes/Core.hs
@@ -1,9 +1,17 @@
 data Ty = TyBool | TyArr Ty Ty
 
-data Term = -- all terms are nameless...
+data Term = -- nameless terms.
     TmTrue
   | TmFalse
   | TmIf Term Term Term
-  | TmVar (Int,Int)
+  | TmVar (Int,Int) -- (dist,ctxlen)
   | TmAbs String Ty Term
   | TmApp Term Term
+
+data NdTerm = -- named terms.
+    NdTrue
+  | NdFalse
+  | NdIf NdTerm NdTerm NdTerm
+  | NdVar String
+  | NdAbs String Ty NdTerm
+  | NdApp NdTerm NdTerm
