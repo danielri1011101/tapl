14e10a7690218132b483a557eba37ac6e6abcdef
danielri1011101 <100494224+danielri1011101@users.noreply.github.com>
2024-08-28 18:09:14 -0500

    Update Lexer.hs

--- simpleTypes/Lexer.hs
+++ simpleTypes/Lexer.hs
@@ -1,7 +1,12 @@
 module Lexer (Token(..), tokenize) where
 
-data Token =
+data Token = -- haven't included the primitive true, false, if!!
     VAR String
+  | IF
+  | THN
+  | ELS
+  | TRU
+  | FLS
   | COLON
   | BOOL
   | ARROW
@@ -21,4 +26,30 @@ tokenize (':':cs) = COLON:tokenize cs
 tokenize ('-':'>':cs) = ARROW:tokenize cs
 tokenize ('B':'o':'o':'l':cs) = BOOL:tokenize cs
 tokenize (c:cs)
-  | c `elem` ['a'..'z'] = getName (c:cs)
+  | c `elem` ['a'..'z'] = let (nm,r) = cutName (c:cs) in dstk nm:tokenize r
+  | c `elem` ['\n',' ','\t'] = tokenize cs
+  | otherwise = [EOF]
+
+dstk :: String -> Token -- decide string token
+dstk "if" = IF
+dstk "then" = THN
+dstk "else" = ELS
+dstk "true" = TRU
+dstk "false" = FLS
+dstk nm = VAR nm
+
+cutName :: String -> (String, String)
+cutName cs = (getName cs, getRdr cs)
+
+getName :: String -> String
+getName =
+  let nameCs = ['a'..'z'] ++ ['0'..'9'] ++ "'"
+  in takeWhile (`elem` nameCs)
+
+
+getRdr :: String -> String 
+getRdr [] = []
+getRdr (c:cs) =
+  let nameCs = ['a'..'z'] ++ ['0'..'9'] ++ "'"
+  in if c `elem` nameCs then getRdr cs
+     else c:cs
